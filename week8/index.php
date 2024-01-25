68dc8553ebea4b3e830fb41b2f0e9c0c5837f015
104302220+Dnzldotexe@users.noreply.github.com <104302220+Dnzldotexe@users.noreply.github.com>
2024-01-25 16:36:57 +0800

    Add wholesome Flareon best pokemon pet

--- week8/index.php
+++ week8/index.php
@@ -214,6 +214,26 @@
     </div>
     <?php
         echo "Fun Fact";
+        echo "
+            Hey guys, 
+            did you know that in terms of human companionship, 
+            Flareon is objectively the most huggable Pokemon? 
+            While their maximum temperature is likely too much for most, 
+            they are capable of controlling it, 
+            so they can set themselves to the perfect temperature for you. 
+            Along with that, 
+            they have a lot of fluff, 
+            making them undeniably incredibly soft to touch. 
+            But that's not all, 
+            they have a very respectable special defense stat of 110, 
+            which means that they are likely very calm and resistant to emotional damage. 
+            Because of this, 
+            if you have a bad day, 
+            you can vent to it while hugging it, 
+            and it won't mind. 
+            It can make itself even more endearing with moves like Charm and Baby Doll Eyes, 
+            ensuring that you never have a prolonged bout of depression ever again.
+        ";
     ?>
     <div style="margin: 2rem 0;">
        <a id="link-hover" href="https://apcwebprog.csf.ph/~dtcempron/lab2/week8/resources.php" target="_blank">Resources</a>
