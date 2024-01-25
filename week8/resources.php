ac55ef808f4585f443562e90f39006c69ee21517
104302220+Dnzldotexe@users.noreply.github.com <104302220+Dnzldotexe@users.noreply.github.com>
2024-01-25 15:13:28 +0800

    Update links to latest apc cloud dns

--- week8/resources.php
+++ week8/resources.php
@@ -24,7 +24,7 @@
 </head>
 <body>
     <h1>
-        <a id="link-hover" href="index.html" target="_blank">
+        <a id="link-hover" href="index.php" target="_blank">
             Danziel Cempron's Personal Profile Website Resources
         </a>
     </h1>
