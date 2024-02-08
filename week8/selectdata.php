b7fe3d5246ef95898e4d9e0aaf7b70c725ad7ba0
104302220+Dnzldotexe@users.noreply.github.com <104302220+Dnzldotexe@users.noreply.github.com>
2024-02-08 15:18:48 +0800

    Replace selected columns

--- week8/selectdata.php
+++ week8/selectdata.php
@@ -1,4 +1,5 @@
 <?php
+//apcwebprog config
 $servername = "localhost";
 $username = "webprogss221";
 $password = "=latHen97";
@@ -11,13 +12,13 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, firstname, lastname FROM dtcempron_myguests";
+$sql = "SELECT id, name, email, website, comment, gender FROM dtcempron_myguests";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    echo "id: " . $row["id"]. " - Name: " . $row["name"]. " ". $row["email"]. " " . $row["website"]. $row["comment"]. $row["gender"]. "      " . "<br>";
   }
 } else {
   echo "0 results";
